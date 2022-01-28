84c44846398ef2035239f3f10661df191fb48221
Sau Juliana <julienyange@gmail.com>
2022-01-28 13:54:53 +0300

    🎉 Initialized react client

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const About = () => {
+  return (
+    <div>
+      <p>About page</p>
+      <Header />
+    </div>
+  );
+};
+
+export default About;
