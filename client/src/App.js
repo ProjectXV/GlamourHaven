11ae08b83a332c2ea3140f8faa493873ffb281b1
Sau Juliana <julienyange@gmail.com>
2022-02-01 23:17:01 +0300

    💄 Finished SignUp page UI

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/client/signup" element={<SignUp />} />
         <Route path="/contacts" element={<Contact />} />
       </Routes>
     </div>
--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -1,79 +1,39 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const Button = {
-  // The styles all button have in common
-  baseStyle: {
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    borderRadius: "base", // <-- border radius is same for all variants and sizes
-    _focus: {
-      outline: "none",
-    },
-  },
-  // Two sizes: sm and md
-  sizes: {
-    sm: {
-      fontSize: "sm",
-      px: 4, // <-- px is short for paddingLeft and paddingRight
-      py: 3, // <-- py is short for paddingTop and paddingBottom
-    },
-    md: {
-      fontSize: "md",
-      px: 6, // <-- these values are tokens from the design system
-      py: 4, // <-- these values are tokens from the design system
-    },
-  },
-  // Two variants: outline and solid
-  variants: {
-    outline: {
-      border: "none",
-      borderColor: "none",
-      color: "purple.500",
-    },
-    solid: {
-      bg: "purple.500",
-      color: "white",
-    },
-  },
-  // The default size and variant values
-  defaultProps: {
-    size: "md",
-    variant: "outline",
-  },
-};
-
 const theme = extendTheme({
-  components: {
-    Button,
+  colors: {
+    neutral: {
+      900: "",
+      800: "",
+      700: "",
+      600: "#242424",
+      500: "#3A3A3A",
+      400: "#8B8B8B",
+      300: "#BEBEBE",
+      200: "#EBEBEB",
+      100: "#F9F9F9",
+    },
+    brand: {
+      500: "#39585D",
+      400: "#70A7A5",
+      300: "#67B6B3",
+      200: "#EFF9F8",
+      100: "",
+    },
   },
-  styles: {
-    colors: {
-      neutral: {
-        900: "",
-        800: "",
-        700: "",
-        600: "#242424",
-        500: "#3A3A3A",
-        400: "#8B8B8B",
-        300: "#BEBEBE",
-        200: "#EBEBEB",
-        100: "#F9F9F9",
+  global: {
+    "a, button": {
+      _focus: {
+        outline: "none",
+        borderColor: "none",
+      },
+      _active: {
+        outline: "none",
+        borderColor: "none",
       },
     },
-    global: {
-      "a, button": {
-        _focus: {
-          outline: "none",
-          borderColor: "none",
-        },
-        _active: {
-          outline: "none",
-          borderColor: "none",
-        },
-      },
-      svg: {
-        color: "#8B8B8B",
-      },
+    svg: {
+      color: "#8B8B8B",
     },
   },
 });
