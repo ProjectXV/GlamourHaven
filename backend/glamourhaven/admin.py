c341f9253054cf7aa15ea6ca196291127bf3d00f
njeveh <njeveh@gmail.com>
2022-02-01 17:50:58 +0300

    registered models to the admin site

--- backend/glamourhaven/admin.py
+++ backend/glamourhaven/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Client)
+admin.site.register(Appointment)
+admin.site.register(Service)
+admin.site.register(Commodity)
