008ffb36ebf994ad1261549900c9655513f7f522
Samson101 <91660168+Samson101@users.noreply.github.com>
2022-02-04 11:51:41 +0300

    created and modified routes, views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('glamourhaven.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('glamourhaven-auth/', include('rest_framework.urls')),
 ]
--- backend/glamourhaven/views.py
+++ backend/glamourhaven/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from django.db.models import fields
+from .serializers import UserSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
